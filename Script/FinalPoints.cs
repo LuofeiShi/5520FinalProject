7f650c975d4b6bbc4970e619657ca8f322387723
tianledong <dtllawrence@gmail.com>
2022-04-29 13:15:36 -0400

    implement final points

--- Script/FinalPoints.cs
+++ Script/FinalPoints.cs
@@ -3,24 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
- public class FinalPoints : MonoBehaviour
- {
-     public static FinalPoints instance = null;
+// public class FinalPoints : MonoBehaviour
+// {
+//     public static FinalPoints instance = null;
 
-     private int finalPoints = 0;
-     void Start()
-     {
-         //PauseMenu point = GetComponent<PauseMenu>();
-         // if (instance == null) {
-         //     instance = this;
-         // }
-         instance = this;
-         UImanager.instance.UpdatePoints(finalPoints);
-     }
+//     private int finalPoints = 0;
+//     void Start()
+//     {
+//         //PauseMenu point = GetComponent<PauseMenu>();
+//         // if (instance == null) {
+//         //     instance = this;
+//         // }
+//         instance = this;
+//         UImanager.instance.UpdatePoints(finalPoints);
+//     }
 
-     public void SetFinalPoints(int finalPoint)
-     {
-         finalPoints = finalPoint;
-         Debug.Log("pointtttttttt");
-     }
- }
+//     public void SetFinalPoints(int finalPoint)
+//     {
+//         finalPoints = finalPoint;
+//         Debug.Log("pointtttttttt");
+//     }
+// }
